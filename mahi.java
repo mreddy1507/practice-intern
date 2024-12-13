f3cc2966c3281ce7d448ddc541d8a464eb0ca6ab
mahesh <mreddy1507@gmail.com>
2024-12-13 13:16:59 +0530

    added some lines in mahi.java

---
+++ mahi.java
@@ -0,0 +1,10 @@
+Working with GIT in the EC2 Instance
+
+Step1: Launch an EC2 Instance
+Step2: Connect to the EC2 instance
+Step3: Login as: ec2-user
+Step4: Switch to root user: sudo su
+Step5: Install git s/w using: yum install git -y
+Step6: To verify: git --version
+			(You should be able to see the git version id)
+

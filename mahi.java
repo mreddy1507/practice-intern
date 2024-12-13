b5b2befa47a35d32fbee9a3fdeadfca65246a333
mahesh <mreddy1507@gmail.com>
2024-12-13 13:20:41 +0530

    added 7891011 lines in mahi.java

--- mahi.java
+++ mahi.java
@@ -6,5 +6,11 @@ Step3: Login as: ec2-user
 Step4: Switch to root user: sudo su
 Step5: Install git s/w using: yum install git -y
 Step6: To verify: git --version
-			(You should be able to see the git version id)
+	(You should be able to see the git version id)
+Step7: Configure username and email id
+Step8: Create a directory
+Step9: Go inside the directory
+Step10: Initialize the git repo
+Step11: Work with GIT commands
+
 
